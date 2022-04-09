f3eabe35add16552360afae285f0ac7c2b7ed2b6
= <=>
2022-04-09 01:49:27 +0000

    Add navbar for small screens

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/NavBar';
 
 function App() {
   return (
-    <div className="mx-24 font-['Poppins']">
+    <div className="font-['Poppins'] mx-8 sm:mx-12 md:mx-16 lg:mx-28 xl:mx-46">
 
       <Header />
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
-
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 
 const Header = () => {
@@ -10,48 +11,32 @@ const Header = () => {
   )
 }
 
-const Logo = () => {
-  return (
-    <a href="#"
-      className="text-lg text-slate-700 capitalize">
-      dental
-      <span className="font-semibold text-sky-500">
-        Care
-      </span>
-    </a>
-  )
-}
-
-const CallToAction = () => {
-  return (
-    <a href="#"
-      className="capitalize px-2 py-1 rounded text-sky-500 border-2 border-sky-500 hover:shadow-lg hover:shadow-sky-500 hover:bg-sky-500 hover:text-slate-100 transition-all duration-300">
-      make appointment
-    </a>
-  )
-}
-
 const NavFull = () => {
   return (
 
-    <nav className='bg-slate-100 flex items-center justify-between px-12 py-4 my-6 mx-12 rounded shadow-lg'>
+    <nav className='bg-slate-100 items-center justify-between px-12 py-4 my-6 rounded shadow-lg hidden lg:flex'>
 
       <Logo />
 
-      <ul className='links flex space-x-2'>
-        <a href="#">
+      <ul className='flex space-x-2'>
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
           home
         </a>
-        <a href="#">
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
           services
         </a>
-        <a href="#">
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
           about
         </a>
-        <a href="#">
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
           gallery
         </a>
-        <a href="#">
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
           contact
         </a>
       </ul>
@@ -63,11 +48,98 @@ const NavFull = () => {
   )
 }
 
-const NavSmall = ({ text }) => {
+const NavSmall = () => {
+
+  const [navLinks, setNavLinks] = useState(false);
+
+  const linkStyles = [
+    'links flex flex-col space-y-2 text-center',
+    'links hidden flex-col space-y-2'
+  ]
+  const navStyles = [
+    'bg-slate-100 flex items-center justify-around px-12 py-4 my-6 rounded shadow-lg lg:hidden',
+    'bg-slate-100 flex items-center justify-between px-12 py-4 my-6 rounded shadow-lg lg:hidden'
+  ]
+
   return (
 
-    <></>
+    <nav className={navLinks ? navStyles[0] : navStyles[1]}>
 
+      <Logo navLinks={navLinks} />
+
+      <ul className={navLinks ? linkStyles[0] : linkStyles[1]}>
+        <Logo className='hidden mb-4' />
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
+          home
+        </a>
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
+          services
+        </a>
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
+          about
+        </a>
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
+          gallery
+        </a>
+        <a href="#"
+          className='text-gray-700 capitalize px-2 py-1 hover:text-sky-500 trans'>
+          contact
+        </a>
+        <CallToAction />
+      </ul>
+
+      <ToggleButton navLinks={navLinks} setNavLinks={setNavLinks} />
+
+    </nav>
+
+  )
+}
+
+const Logo = ({ navLinks=false }) => {
+
+  const styles = [
+    'text-lg text-slate-700 capitalize hidden',
+    'text-lg text-slate-700 capitalize block'
+  ]
+
+  return (
+    <a href="#"
+      className={navLinks ? styles[0] : styles[1]}>
+      dental
+      <span className="font-semibold text-sky-500">
+        Care
+      </span>
+    </a>
+  )
+}
+
+const CallToAction = () => {
+  return (
+    <a href="#"
+      className="capitalize px-2 py-1 rounded text-sky-500 border-2 border-sky-500 hover:shadow-lg hover:shadow-sky-500 hover:bg-sky-500 hover:text-slate-100 trans">
+      make appointment
+    </a>
+  )
+}
+
+const ToggleButton = ({navLinks, setNavLinks}) => {
+
+  const bars = 'text-2xl text-slate-700 hover:text-sky-500 trans cursor-pointer';
+  const ex = 'text-2xl text-slate-700 hover:text-sky-500 trans cursor-pointer absolute top-[2.65rem] right-[5rem]';
+  
+  const onClick = () => setNavLinks(prev => !prev);
+  
+  return (
+    <>
+    <FaBars onClick={onClick}
+    className={navLinks ? 'hidden' : bars} />
+    <FaTimes onClick={onClick}
+    className={navLinks ? ex : 'hidden'} />
+    </>
   )
 }
 
