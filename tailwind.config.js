9699f2084fd75d8a1189955fec9b0732f8d73dc6
ayoubmazouz <ayoub2000mazouz@gmail.com>
2022-06-19 15:12:14 +0100

    adding font in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,28 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
-  theme: {
-    extend: {
-      colors: {
-        dark: '#343536',
-        light: '#FFFFFF',
-        primary: '#005FA0',
-        secondary: '#FE8573',
-        'light-blue': '#0091FF',
-        'light-gray': '#4d606e',
-        'bluish-gray': '#F8FCFF',
+	content: ["./src/**/*.{js,jsx}"],
+	theme: {
+		extend: {
+			colors: {
+				dark: "#343536",
+				light: "#FFFFFF",
+				primary: "#005FA0",
+				secondary: "#FE8573",
+				"light-blue": "#0091FF",
+				"light-gray": "#4d606e",
+				"bluish-gray": "#F8FCFF",
 
-        // Social MEdia Colors.
-        facebook: "#1877F2",
-        twitter: "#1DA1F2",
-        linkedin: "#0A66C2",
-        youtube: "#CD201F",  
-        instagram: "#E1306C",
-        whatsapp: "#25D366",
-      }
-    },
-  },
-  plugins: [],
+				// Social MEdia Colors.
+				facebook: "#1877F2",
+				twitter: "#1DA1F2",
+				linkedin: "#0A66C2",
+				youtube: "#CD201F",
+				instagram: "#E1306C",
+				whatsapp: "#25D366",
+			},
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 }
